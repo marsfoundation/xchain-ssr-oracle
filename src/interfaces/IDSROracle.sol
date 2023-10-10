9a24ff507ae0854234fb63348ecb679a7301f8bd
Sam MacPherson <xylom44@gmail.com>
2023-10-10 06:13:28 -0400

    add natspec to interface

--- src/interfaces/IDSROracle.sol
+++ src/interfaces/IDSROracle.sol
@@ -13,26 +13,76 @@ interface IDSROracle {
         uint40  rho;    // Last computed timestamp [seconds]
     }
 
+    /**
+     * @notice Retrieve the current PotData: dsr, chi, and rho.
+     * @return The current PotData struct.
+     */
     function getPotData() external view returns (PotData memory);
 
+    /**
+     * @notice Get the current Dai Savings Rate.
+     * @return The Dai Savings Rate in per-second value [ray].
+     */
     function getDSR() external view returns (uint256);
 
+    /**
+     * @notice Get the last computed conversion rate.
+     * @return The last computed conversion rate [ray].
+     */
     function getChi() external view returns (uint256);
 
+    /**
+     * @notice Get the last computed timestamp.
+     * @return The last computed timestamp [seconds].
+     */
     function getRho() external view returns (uint256);
 
+    /**
+     * @notice Get the Annual Percentage Rate.
+     * @return The APR.
+     */
     function getAPR() external view returns (uint256);
 
+    /**
+     * @notice Get the conversion rate at the current timestamp.
+     * @return The conversion rate.
+     */
     function getConversionRate() external view returns (uint256);
 
+    /**
+     * @notice Get the conversion rate at a specified timestamp.
+     * @dev    Timestamp must be greater than or equal to the current timestamp.
+     * @param  timestamp The timestamp at which to retrieve the conversion rate.
+     * @return The conversion rate.
+     */
     function getConversionRate(uint256 timestamp) external view returns (uint256);
 
+    /**
+     * @notice Get the binomial approximated conversion rate at the current timestamp.
+     * @return The binomial approximated conversion rate.
+     */
     function getConversionRateBinomialApprox() external view returns (uint256);
 
+    /**
+     * @notice Get the binomial approximated conversion rate at a specified timestamp.
+     * @dev    Timestamp must be greater than or equal to the current timestamp.
+     * @param  timestamp The timestamp at which to retrieve the binomial approximated conversion rate.
+     * @return The binomial approximated conversion rate.
+     */
     function getConversionRateBinomialApprox(uint256 timestamp) external view returns (uint256);
 
+    /**
+     * @notice Get the linear approximated conversion rate at the current timestamp.
+     * @return The linear approximated conversion rate.
+     */
     function getConversionRateLinearApprox() external view returns (uint256);
 
+    /**
+     * @notice Get the linear approximated conversion rate at a specified timestamp.
+     * @dev    Timestamp must be greater than or equal to the current timestamp.
+     * @param  timestamp The timestamp at which to retrieve the linear approximated conversion rate.
+     * @return The linear approximated conversion rate.
+     */
     function getConversionRateLinearApprox(uint256 timestamp) external view returns (uint256);
 
 }
