652580544341a0c812fa0d53a68d9b31c6efb99f
Sam MacPherson <xylom44@gmail.com>
2024-03-29 02:02:22 +0900

    compare to sdai value

--- test/DSROracleXChainIntegrationBase.t.sol
+++ test/DSROracleXChainIntegrationBase.t.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 
+import { IERC4626 } from "forge-std/interfaces/IERC4626.sol";
+
 import { BridgedDomain, Domain } from "xchain-helpers/testing/BridgedDomain.sol";
 
 import { DSRAuthOracle  }                  from "../src/DSRAuthOracle.sol";
@@ -22,7 +24,8 @@ abstract contract DSROracleXChainIntegrationBaseTest is Test {
     Domain mainnet;
     BridgedDomain remote;
 
-    address pot = 0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
+    address pot  = 0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7;
+    address sdai = 0x83F20F44975D03b1b09e64809B757c47f942BEeA;
 
     DSRAuthOracle oracle;
 
@@ -57,17 +60,23 @@ abstract contract DSROracleXChainIntegrationBaseTest is Test {
 
         mainnet.selectFork();
 
+        // Anchor the time to the RHO so we can check the hard coded value
+        vm.warp(CURR_RHO + 30 days);
+
+        uint256 sdaiConversionRate = IERC4626(sdai).convertToAssets(1e18);
+        assertEq(sdaiConversionRate, 1.044120771690352453e18);
+
         doRefresh();
 
         remote.relayFromHost(true);
+        vm.warp(CURR_RHO + 30 days);
 
         assertEq(oracle.getDSR(), CURR_DSR);
         assertEq(oracle.getChi(), CURR_CHI);
         assertEq(oracle.getRho(), CURR_RHO);
 
-        // Anchor the time to the RHO so we can check the hard coded value
-        vm.warp(CURR_RHO + 30 days);
         assertEq(balancerAdapter.getRate(), 1.044120769556671376e18);
+        assertApproxEqAbs(balancerAdapter.getRate(), sdaiConversionRate, 1e10);
     }
 
 }
