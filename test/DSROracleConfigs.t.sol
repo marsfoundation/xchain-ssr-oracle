541546c1397ecf3c3d8c3d990b2ae5230799553c
lucas-manuel <lucasmanuel.tech@gmail.com>
2024-04-09 08:31:15 -0400

    feat: use registry, add first assertions

--- test/DSROracleConfigs.t.sol
+++ test/DSROracleConfigs.t.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 
-import { Ethereum } from "sparklend-address-registry/Ethereum.sol";
+import { Ethereum } from "lib/sparklend-address-registry/src/Ethereum.sol";
+import { Optimism } from "lib/sparklend-address-registry/src/Optimism.sol";
 
 import { PotMock } from "./mocks/PotMock.sol";
 
@@ -17,19 +18,18 @@ contract DSROracleConfigs is Test {
 // Balancer Rate Provider (Optimism): [0x15ACEE5F73b36762Ab1a6b7C98787b8148447898](https://optimistic.etherscan.io/address/0x15ACEE5F73b36762Ab1a6b7C98787b8148447898#code)
 
     function test_optimism_deployment() public {
-        DSROracleForwarderOptimism forwarderEthereum = DSROracleForwarderOptimism(0x4042127DecC0cF7cc0966791abebf7F76294DeF3);
+        DSROracleForwarderOptimism forwarderEthereum = DSROracleForwarderOptimism(Ethereum.DSR_FORWARDER_OPTIMISM);
 
         address authOracleL2           = 0x33a3aB524A43E69f30bFd9Ae97d1Ec679FF00B64;
         address receiverL2             = 0xE206AEbca7B28e3E8d6787df00B010D4a77c32F3;
         address balancerRateProviderL2 = 0x15ACEE5F73b36762Ab1a6b7C98787b8148447898;
 
-        vm.createSelectFork(getChain('optimism').rpcUrl, 19618011);  // April 9, 2024
+        vm.createSelectFork(getChain('mainnet').rpcUrl, 19618011);  // April 9, 2024
 
-        assertEq(forwarderEthereum.l2Oracle(), receiverL2);
-        assertEq(forwarderEthereum.pot(),      Ethereum.Pot);
+        assertEq(forwarderEthereum.l2Oracle(),     receiverL2);
+        assertEq(address(forwarderEthereum.pot()), Ethereum.POT);
 
-        vm.createSelectFork(getChain('mainnet').rpcUrl, 118532925);  // April 9, 2024
-
+        vm.createSelectFork(getChain('optimism').rpcUrl, 118532925);  // April 9, 2024
 
     }
 }
