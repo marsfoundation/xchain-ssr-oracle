f53fa89fb37e75d91a4a18862585f21347c39a3f
Sam MacPherson <xylom44@gmail.com>
2024-08-21 08:00:50 -0400

    comment change; use safe casts for pot data

--- src/forwarders/DSROracleForwarderBase.sol
+++ src/forwarders/DSROracleForwarderBase.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.0;
 
+import { SafeCast } from "openzeppelin-contracts/contracts/utils/math/SafeCast.sol";
+
 import { IDSRAuthOracle, IDSROracle } from '../interfaces/IDSRAuthOracle.sol';
 import { IPot }                       from '../interfaces/IPot.sol';
 
@@ -9,6 +11,8 @@ import { IPot }                       from '../interfaces/IPot.sol';
  * @notice Base contract for relaying pot data messages cross-chain.
  */
 abstract contract DSROracleForwarderBase {
+
+    using SafeCast for uint256;
 
     event LastSeenPotDataUpdated(IDSROracle.PotData potData);
 
@@ -24,9 +28,9 @@ abstract contract DSROracleForwarderBase {
 
     function _packMessage() internal returns (bytes memory) {
         IDSROracle.PotData memory potData = IDSROracle.PotData({
-            dsr: uint96(pot.dsr()),
-            chi: uint120(pot.chi()),
-            rho: uint40(pot.rho())
+            dsr: pot.dsr().toUint96(),
+            chi: pot.chi().toUint120(),
+            rho: pot.rho().toUint40()
         });
         _lastSeenPotData = potData;
         emit LastSeenPotDataUpdated(potData);
