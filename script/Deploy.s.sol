d3125e56404b1f310e502d533bff0ccdbc8a20ca
oldchili <130549691+oldchili@users.noreply.github.com>
2026-04-01 17:10:46 +0700

    Move comment

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -50,10 +50,11 @@ contract Deploy is Script {
 
         // Configure
         oracle.grantRole(oracle.DATA_PROVIDER_ROLE(), receiver);
+
+        // Note that if admin role is entirely revoked maxSSR can not be set
         if (admin != address(0)) {
             oracle.grantRole(oracle.DEFAULT_ADMIN_ROLE(), admin);
         }
-        // Note that if admin role is entirely revoked maxSSR can not be set
         oracle.renounceRole(oracle.DEFAULT_ADMIN_ROLE(), deployer);
         vm.stopBroadcast();
 
